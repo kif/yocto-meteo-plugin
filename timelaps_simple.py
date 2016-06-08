d33bd50a2a06d55b2f38f15953be4d57e9592b7c
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2016-06-08 15:18:56 +0200

    separate averageing of WB (longer hours to days) and exposure time (max 10 mn)

--- timelaps_simple.py
+++ timelaps_simple.py
@@ -13,85 +13,123 @@ import math
 import fractions
 import json
 
-resolution = (2592, 1944)
-#resolution = (1296, 972)
-fps = 1
-delay = 360 # 10 images per hour
-#delay = 5
-folder = "."
+class TimeLaps(object):
+    def __init__(self, resolution=(2592, 1944), fps=1, delay=360, 
+                 folder=".", avg_gains=200, avg_speed=5, config_file="parameters.json"):
+        self.resolution = resolution
+        self.fps = fps
+        self.delay = delay
+        self.folder = os.path.abspath(folder)
+        self.avg_gains = avg_gains # time laps over which average gains
+        self.avg_speeds = avg_speed # time laps over which average speed
+        self.config_file = os.path.abspath(config_file)
+        self.red_gains = []
+        self.blue_gains = []
+        self.speeds = []
+	self.load_config()
+        self.camera = PiCamera(resolution=self.resolution, framerate=self.fps) 
+        self.camera.awb_mode = "auto"
+        self.camera.iso = 0
+        self.camera.shutter_speed = 0
+        self.last_gain = 1
+        self.last_speed = 0
+        self.last_wb = (1,1)
+        self.next_speed = 1
+        self.next_wb = (1,1)
+        self.last_img = time.time()
+        self.next_img = self.last_img + self.delay
 
-nb_gains = 20 #transition over 2hours
-config_file = "parameters.json"
-if os.path.isfile(config_file):
-    with open(config_file) as jsonfile:
-        dico = json.load(jsonfile)
-    red_gains = dico.get("red_gains", [])
-    blue_gains = dico.get("blue_gains", [])
-    speeds = dico.get("speeds", [])
-else:
-    red_gains = []
-    blue_gains = []
-    speeds = []
+    def __del__(self):
+        self.camera.close()
+        self.camera = None
 
+    def load_config(self):
+        if os.path.isfile(self.config_file):
+            with open(self.config_file) as jsonfile:
+                dico = json.load(jsonfile)
+            self.red_gains = dico.get("red_gains", self.red_gains)
+            self.blue_gains = dico.get("blue_gains", self.blue_gains)
+            self.speeds = dico.get("speeds", self.speeds)
 
-def save(ary, name):
-    "save an array"
-    Image.fromarray(ary).save(name)
+    def save_config(self):
+        dico = {"red_gains": self.red_gains, 
+                "blue_gains": self.blue_gains,
+                "speeds": self.speeds}
 
-def capture(cam):
-    """
-    Take a picture with the camera
-    """
-    fname = datetime.datetime.now().strftime("%Y-%m-%d-%Hh%Mm%Ss.jpg")
-    print(fname)
-    cam.capture(fname)
+        with open(self.config_file,"w") as jsonfile:
+            jsonfile.write(json.dumps(dico, indent=2))
 
-def calc_awb(camera, red_gains, blue_gains, speeds):
-    camera.awb_mode = 'auto'
-    camera.shutter_speed = 0
-    my_stream = io.BytesIO()
-    camera.capture(my_stream, 'jpeg')
-    r,b = camera.awb_gains
-    speed = camera.exposure_speed
-    r = 1.0*r.numerator/r.denominator
-    b = 1.0*b.numerator/b.denominator
-    ls = (1.0*speed.numerator/speed.denominator)
-    red_gains.append(r)
-    blue_gains.append(b)
-    speeds.append(math.log(ls, 2.0))
-    if len(red_gains)>nb_gains:
-        red_gains = red_gains[-nb_gains:]
-        blue_gains = blue_gains[-nb_gains:]
-        speeds = speeds[-nb_gains:]
-    rg = sum(red_gains)/len(red_gains)
-    bg = sum(blue_gains)/len(blue_gains)
-    ns = max(1,int(round(math.pow(2.0, sum(speeds)/len(speeds)))))
-    print("len %s, \t S %s -> %s \t R %s-> %s \t B %s -> %s"%(len(red_gains), ls, ns, r,rg, b, bg))
-    camera.awb_mode = 'off'
-    camera.awb_gains = rg, bg
-    camera.shutter_speed = int(ns)
-    with open(config_file,"w") as jsonfile:
-        jsonfile.write(json.dumps({"red_gains":red_gains, "blue_gains": blue_gains, "speeds": speeds}, indent=2))
-
+    def save(self, ary, name):
+        "save an array"
+        Image.fromarray(ary).save(name)
     
-    
+    def capture(self):
+        """Take a picture with the camera, if there is enough light
+        """
+        if not self.is_valid():
+            print("Skipped, low light Speed %.3f/iso%.0f"%(self.last_speed, self.last_gain))
+            return     
+        ns = self.next_speed
+        iso = 100
+        while (ns < 4) and (iso < 800):
+            iso *= 2
+            ns *= 2
+        if ns < 1.0:
+            ns = 1.0
+        if iso > 800:
+            iso = 800
+        self.camera.awb_mode = 'off'
+        self.camera.awb_gains = self.next_wb
+        self.camera.iso = iso
+        self.camera.shutter_speed = int(round(1e6/ns))
+        self.last_img = time.time()
+        self.next_img = self.last_img + self.delay
+        fname = datetime.datetime.fromtimestamp(self.last_img).strftime("%Y-%m-%d-%Hh%Mm%Ss.jpg")
+        print("%s s/%.3f iso %i, expected speed=%.3f R=%.3f B=%.3f"%(fname, ns, iso, self.next_speed, self.next_wb[0], self.next_wb[1]))
+        self.camera.capture(fname)
+        self.camera.awb_mode = "auto"
+        self.camera.iso = 0
+        self.camera.shutter_speed = 0
 
-with PiCamera(resolution=resolution, framerate=fps) as camera:
-    t0 = time.time()
-    last = None
-    timestamp = 0
-#    camera.start_preview()
+    def measure(self):   
+        my_stream = io.BytesIO()
+        self.camera.capture(my_stream, 'jpeg')
+        r,b = self.camera.awb_gains
+        self.last_speed = 1.0e6 / self.camera.exposure_speed
+        self.last_gain = 1.0 * self.camera.analog_gain * self.camera.digital_gain
+        if not self.is_valid():
+            return
+        r = 1.0 * r
+        b = 1.0 * b
+        self.last_wb = (r, b)
+        ls = self.last_speed / self.last_gain
+        self.red_gains.append(r)
+        self.blue_gains.append(b)
+        self.speeds.append(math.log(ls, 2.0))
+        if len(self.red_gains) > self.avg_gains:
+            red_gains = red_gains[-self.avg_gains:]
+            blue_gains = blue_gains[-self.avg_gains:]
+        if len(self.speeds) > self.avg_speeds:
+           self.speeds = self.speeds[-self.avg_speeds:]
+        rg = sum(self.red_gains)/len(self.red_gains)
+        bg = sum(self.blue_gains)/len(self.blue_gains)
+        ns = 2.0 ** (sum(self.speeds)/len(self.speeds))
+        print("len %s/%s, \t S %.3f/iso%.0f -> %.3f \t R %.3f-> %.3f \t B %.3f -> %.3f"%(len(self.red_gains),len(self.speeds), self.last_speed, 100.*self.last_gain, ns, r, rg, b, bg))
+        self.next_speed = ns
+        self.next_wb = rg,bg
+        self.save_config()
+
+    def is_valid(self):
+       return self.last_speed/self.last_gain >=1.5 
+    
+if __name__ == "__main__":
+    tl = TimeLaps()
     print("Warmup ...")
     time.sleep(2)
-    #capture(camera)
-    calc_awb(camera, red_gains, blue_gains, speeds)
-    red_gains += [red_gains[-1]] * (nb_gains - len(red_gains))
-    blue_gains += [blue_gains[-1]] * (nb_gains - len(blue_gains))
-    speeds += [speeds[-1]] * (nb_gains - len(speeds))
+    tl.measure()
     while True:
-        next = time.time() + delay
-        calc_awb(camera, red_gains, blue_gains, speeds)
-        capture(camera)
-        while time.time() < next:
-            time.sleep(1)
+        tl.capture()
+        while time.time() < tl.next_img:
+            time.sleep(2.0*tl.delay/tl.avg_speeds)
+            tl.measure()
 
