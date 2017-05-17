d236b7baa4db75ad4960ebfe247bd10f698f2eb6
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2017-05-17 21:34:45 +0200

    add spec for a few servos

--- servo.py
+++ servo.py
@@ -11,6 +11,13 @@ pwm.set_pwm_freq(FREQ)
 
 class Servo(object):
     """Represent a servomotor"""
+    PROTECT = False 
+    #set to True if the range should be restricted to protect the servo
+
+    # Specification of the servo deg -> us
+    SPEC={  4: 581,#us
+            17: 2380}
+
     def __init__(self, pin, offset=0, reverse=False):
         """
         :param pin: pin number on the PCA9685 card
@@ -19,12 +26,17 @@ class Servo(object):
         self.pin = pin
         self.offset = offset
         self.reverse = reverse
-        self.slope = 0
-        self.inter = 0
+        angles = list(self.SPEC.keys())
+        amax = max(angles)
+        amin = min(angles)
+        tick = 1.0e6 / FREQ / RESOLUTION
+        tickmax = self.SPEC[amax] / tick
+        tickmin = self.SPEC[amin] / tick
+        self.slope = (tickmax - tickmin) / (amax - amin)
+        self.inter = tickmin - amin * self.slope 
 
     def move(self, angle):
         ticks = self.calc(angle)
-        #print("ticks: %s"%ticks)
         pwm.set_pwm(self.pin, 0, int(round(ticks)))
 
     def off(self):
@@ -35,61 +47,38 @@ class Servo(object):
             angle=-angle
         if self.offset:
             angle -= self.offset
+        if self.PROTECT:
+            #Protect for out of bounds ...
+            angles = list(self.SPEC.keys())
+            angle = max(angle, min(angles))
+            angle = min(angle, max(angles))
         return int(round(self.inter + self.slope*angle))
 
 
-class SG90(Servo):
-    SPEC={  4: 581,#us
-            176: 2380}
-    def __init__(self, pin, offset=0, reverse=False):
-        Servo.__init__(self, pin, offset=offset, reverse=reverse)
-        angles = list(self.SPEC.keys())
-        amax = max(angles)
-        amin = min(angles)
-        tick = 1.0e6 / FREQ / RESOLUTION
-        tickmax = self.SPEC[amax] / tick
-        tickmin = self.SPEC[amin] / tick
-        self.slope = (tickmax - tickmin) / (amax - amin)
-        self.inter = tickmin - amin * self.slope 
+SG90 = Servo
 
 
-class M15S(SG90):
+class M15S(Servo):
+    # deg -> us
     SPEC = { -90: 482,
             180: 2604,}
 
 
-class MG90s(SG90):
+class MG90s(Servo):
+    # deg -> us
     SPEC={  4: 581,#us
           176: 2380}
 
+class MG92b(Servo):
+    SPEC = { 14.300477656208258: 752.7669270833334,# deg -> us
+             165.69952234379173: 2469.889322916667}
 
 class TSS11MGB(Servo):
     SPEC={  2.17: 691.732,#deg->us
             175.23: 2685.55}
-    def __init__(self, pin, offset=0, reverse=False):
-        Servo.__init__(self, pin, offset=offset, reverse=reverse)
-        angles = list(self.SPEC.keys())
-        amax = max(angles)
-        amin = min(angles)
-        tick = 1.0e6 / FREQ / RESOLUTION
-        tickmax = self.SPEC[amax] / tick
-        tickmin = self.SPEC[amin] / tick
-        self.slope = (tickmax - tickmin) / (amax - amin)
-        self.inter = tickmin - amin * self.slope
-
-    def calc(self, angle):
-        if self.reverse:
-            angle=-angle
-        if self.offset:
-            angle -= self.offset
-        #Protect for out of bounds ...
-        angles = list(self.SPEC.keys())
-        angle = max(angle, min(angles))
-        angle = min(angle, max(angles))
-        #print(angle)
-        return int(round(self.inter + self.slope*angle))
+    PROTECT = True
 
 pan = M15S(14, reverse=True, offset=-90)
-tilt = SG90(15, reverse=False, offset=-90)
-mg90 = MG90s(13, reverse=False, offset=0)
-tss = TSS11MGB(12)
+#tilt = SG90(15, reverse=False, offset=-90)
+tilt = MG92b(15, reverse=False, offset=-80)
+#tss = TSS11MGB(12)
